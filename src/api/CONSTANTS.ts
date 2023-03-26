f3900b82bab941a15bbde416e954223521dec98c
Leky90 <leky90@users.noreply.github.com>
2023-03-26 16:11:46 +0700

    update api domain

--- src/api/CONSTANTS.ts
+++ src/api/CONSTANTS.ts
@@ -1,4 +1,4 @@
-const BASE_API = "https://pokemon-game.ldktech.com/websocket";
+const BASE_API = "https://pokemon-socket.ldktech.com";
 // const BASE_API = "http://localhost:8080";
 
 export default BASE_API;
